e6f66ae4b6fe127c21921c91a817b9f76e830312
S Praveen <108536707+Spraveen8-chary@users.noreply.github.com>
2024-10-20 20:59:52 +0530

    Add files via upload

---
+++ features/apikey.py
@@ -0,0 +1,12 @@
+geminiai_api_key = "AIzaSyBy_b2VIh-hk2y7m2qawgzDJQT9tt8GvBo"
+
+gemini_api_key = "AIzaSyCksPVjZjGql6E3JnenSturpjIRf_PUYAA"
+
+#streamlit run --server.port 8000 app.py
+
+GOOGLE_API_KEY = "AIzaSyDK1iaegYGIgYS1vY3IPR8Rqv2fFLbpTis"
+
+SEARCH_ID = "23c2823da75e1409f"
+
+YOUTUBE_API_KEY = "AIzaSyCyq_sYCGnNydRQyQ0tTVYYZHtAhfHArdY"
+# AIzaSyAxU12PI3Mfqf9qOUXssZnjLlo9cLQoqlQ
